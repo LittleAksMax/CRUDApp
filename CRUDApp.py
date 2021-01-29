23feacea7e7485a72a17dc46b275ec2af4706b5b
David R <RosentalD16@chesterton.cambs.sch.uk>
2021-01-29 18:26:43 +0000

    Created all the html files, created/perfected sign-in and log-in sheet. Added all flask functions in the CRUDApp.py file.

--- CRUDApp.py
+++ CRUDApp.py
@@ -4,11 +4,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello, World!</h1>"
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/join")
+def join():
+    return render_template("signup.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return ""
+
+@app.route("/<usr>/insert")
+def insert():
+    return render_template("insert.html")
+
+@app.route("/<usr>/update")
+def update():
+    return render_template("update.html")
+
+@app.route("/<usr>/delete")
+def delete():
+    return render_template("delete.html")
 
 if __name__ == "__main__":
     app.run()
