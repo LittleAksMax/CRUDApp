1a5c0da2772744416b2b5743637529651c0a051a
David R <RosentalD16@chesterton.cambs.sch.uk>
2021-01-29 19:59:59 +0000

    Perfected log in and sign ups

--- CRUDApp.py
+++ CRUDApp.py
@@ -1,34 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", stylesheet=False)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", stylesheet=True)
 
 @app.route("/join")
 def join():
-    return render_template("signup.html")
+    return render_template("signup.html", stylesheet=True)
 
 @app.route("/<usr>")
-def user(usr):
+def user(usr, stylesheet=True):
     return ""
 
 @app.route("/<usr>/insert")
 def insert():
-    return render_template("insert.html")
+    return render_template("insert.html", stylesheet=True)
 
 @app.route("/<usr>/update")
 def update():
-    return render_template("update.html")
+    return render_template("update.html", stylesheet=True)
 
 @app.route("/<usr>/delete")
 def delete():
-    return render_template("delete.html")
+    return render_template("delete.html", stylesheet=True)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
