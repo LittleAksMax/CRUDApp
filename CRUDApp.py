b3d0e386c61def33a759dea7a2d70be6ace7d475
David R <RosentalD16@chesterton.cambs.sch.uk>
2021-01-31 19:47:25 +0000

    Navbar set up, still needs perfection, but links work]#

--- CRUDApp.py
+++ CRUDApp.py
@@ -3,43 +3,54 @@ from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "LittleAksMax"
-session.permanent = True
 app.permanent_session_lifetime = timedelta(hours=5)
 
 @app.route("/")
 def index():
-    return render_template("index.html", stylesheet=False)
+    if "user" not in session:
+        return render_template("index.html", session=session)
+    else:
+        return redirect(url_for("user", usr=session["user"]))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method != "POST":
-        return render_template("login.html", stylesheet=True)
+        return render_template("login.html")
 
     user = request.form["username"]
     passwd = request.form["password"]
+    session["user"] = user # create session
 
-    # TODO: series of checks
+    # TODO: series of checks on entered credentials
     return redirect(url_for("user", usr=user))
 
 @app.route("/join")
 def join():
-    return render_template("signup.html", stylesheet=True)
+    return render_template("signup.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None) # remove data
+    return redirect(url_for("index"))
 
 @app.route("/<usr>")
-def user(usr, stylesheet=True):
-    return render_template("user.html")
+def user(usr):
+    if "user" not in session:
+        return redirect(url_for("index"))
+    else:
+        return render_template("user.html")
 
 @app.route("/<usr>/insert")
 def insert():
-    return render_template("insert.html", stylesheet=True)
+    return render_template("insert.html")
 
 @app.route("/<usr>/update")
 def update():
-    return render_template("update.html", stylesheet=True)
+    return render_template("update.html")
 
 @app.route("/<usr>/delete")
 def delete():
-    return render_template("delete.html", stylesheet=True)
+    return render_template("delete.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
