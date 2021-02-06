ac8a0cc0e3f51fa2f8306af57a46390f7215870b
littleaksmax <david2005.rosental@gmail.com>
2021-02-06 23:55:52 +0000

    Created buttons, just need to program up the functionality of delete, add, and edit; also search box not done yet, along with the filter function in database_handler.py

--- CRUDApp.py
+++ CRUDApp.py
@@ -83,15 +83,15 @@ def user(usr):
         return render_template("user.html", usrname=usr, employees=session["emps"])
 
 @app.route("/<usr>/insert")
-def insert():
+def insert(usr):
     return render_template("insert.html")
 
 @app.route("/<usr>/update")
-def update():
+def update(usr):
     return render_template("update.html")
 
 @app.route("/<usr>/delete")
-def delete():
+def delete(usr):
     return render_template("delete.html")
 
 if __name__ == "__main__":
